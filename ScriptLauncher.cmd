af9b6cf3538a4e564194b7cc090775a769b75abe
needkg <48919928+needkg@users.noreply.github.com>
2024-10-10 09:58:44 -0300

    Delete ScriptLauncher.cmd

--- ScriptLauncher.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 >nul
-title Script feito por Claudinei Junior
-
-:main
-mkdir "%APPDATA%\Claudinei" >nul 2>&1
-curl -L "https://raw.githubusercontent.com/needkg/uninga-lab-script/refs/heads/main/ScriptSource.cmd?token=%RANDOM%" -o "%APPDATA%\Claudinei\Script.cmd"
-start "" "%APPDATA%\Claudinei\Script.cmd" >nul 2>&1
-exit
