ade4fc27f4c8a620e8ec4dd5fae10d46aa75c5a1
needkg <48919928+needkg@users.noreply.github.com>
2024-10-10 09:23:47 -0300

    Update ScriptLauncher.cmd

--- ScriptLauncher.cmd
+++ ScriptLauncher.cmd
@@ -5,5 +5,5 @@ title Script feito por Claudinei Junior
 :main
 mkdir "%APPDATA%\Claudinei" >nul 2>&1
 curl -L "https://raw.githubusercontent.com/needkg/uninga-lab-script/refs/heads/main/ScriptSource.cmd" -o "%APPDATA%\Claudinei\Script.cmd" >nul 2>&1
-start /min "" "%APPDATA%\Claudinei\Script.bat"
+start /min "" "%APPDATA%\Claudinei\Script.cmd"
 exit
