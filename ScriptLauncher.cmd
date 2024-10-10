b5beb32c2484f18a59330c3410e31d9c3ebaf6b3
needkg <48919928+needkg@users.noreply.github.com>
2024-10-10 09:16:39 -0300

    Create ScriptLauncher.cmd

---
+++ ScriptLauncher.cmd
@@ -0,0 +1,9 @@
+@echo off
+chcp 65001 >nul
+title Script feito por Claudinei Junior
+
+:main
+mkdir "%APPDATA%\Claudinei" >nul 2>&1
+curl -L "https://raw.githubusercontent.com/needkg/uninga-lab-script/refs/heads/main/ScriptSource.cmd" -o "%APPDATA%\Claudinei\Script.bat" >nul 2>&1
+start /min "" "%APPDATA%\Claudinei\Script.bat"
+exit
